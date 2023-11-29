d46e4ba9f981796ddb650e38e1c3e7a31e60d4ec
Tayfun YUGRUK <tayfunyugruk@gmail.com>
2023-11-29 03:36:50 -0500

    index.html improved

--- Greeting.js
+++ Greeting.js
@@ -5,35 +5,79 @@ function Greeting({ name }) {
   const [showDate, setShowDate] = useState(true);
 
   console.log("enabled", enabled);
+  console.log("showDate", showDate);
 
   if (enabled) {
-    return createElement("div", { className: "padding-20", __innerHTML: "Content removed" });
-  }
-
-  let preparedElement = null;
-  if (showDate) {
-    preparedElement = [
+    return [
+      createElement("div", { className: "padding-20", __innerHTML: "Page 3" }),
       createElement(
         "button",
         {
           className: "btn-primary",
           events: {
             click: (evt) => {
-              // setEnabled(true);
+              setEnabled(false);
+              console.log("onclick");
+            }
+          }
+        },
+        createElement("div", { __innerHTML: `Go to page 1 ?` })
+      ),
+    ];
+  }
+
+  let preparedElement = null;
+  if (showDate) {
+    preparedElement = [
+      createElement("div", { __innerHTML: `Page 1` }),
+      createElement(
+        "button",
+        {
+          className: "btn-primary",
+          events: {
+            click: (evt) => {
+              setShowDate(false);
+              console.log("onclick");
+            }
+          }
+        },
+        createElement("div", { __innerHTML: `Go to page 2 ${name}?` })
+      ),
+      createElement("div", { __innerHTML: `${new Date().getTime()}` })
+    ];
+  } else {
+    preparedElement = [
+      createElement("div", { className: "", __innerHTML: "Page 2" }),
+      createElement(
+        "button",
+        {
+          className: "btn-primary",
+          events: {
+            click: (evt) => {
               setShowDate(true);
               console.log("onclick");
             }
           }
         },
-        createElement("div", { __innerHTML: `Do you want to toggle ${name}?` })
+        createElement("div", { __innerHTML: `Revert back` })
       ),
-      createElement("div", { __innerHTML: `${new Date().getTime()}` })
+      createElement(
+        "button",
+        {
+          className: "btn-primary",
+          events: {
+            click: (evt) => {
+              setEnabled(true);
+              console.log("onclick");
+            }
+          }
+        },
+        createElement("div", { __innerHTML: `Go to page 3` })
+      ),
     ];
-  } else {
-    preparedElement = createElement("span", { __innerHTML: `Do you want to re-render ${name}?` });
   }
 
-  return createElement("h1", { className: "greeting" }, preparedElement);
+  return createElement("h1", { className: "padding-20" }, preparedElement);
 }
 
 export default Greeting;
